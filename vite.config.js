e71b9c8bd661737cf54cdc86865c9e833cdd7562
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2024-08-20 21:25:17 +0530

    added workflow

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://react-news24x7-frontend.vercel.app/'
+  base: '/ReactNews24x7-FrontEnd/'
 })
